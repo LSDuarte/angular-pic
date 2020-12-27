c597335247ff09553d3a539b65ab9052b7959929
Larissa Duarte <42499@pjmt.local>
2020-12-27 11:44:42 -0400

    finalizando intradução ao angular - parte 1

--- src/app/photos/photo-list/photo-list-module.ts
+++ src/app/photos/photo-list/photo-list-module.ts
@@ -1,24 +1,28 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { DarkenOnHoverModule } from "../../shared/directives/darken-on-hover/darken-on-hover.module";
 import { CardModule } from "../../shared/components/card/card.module";
 import { PhotoModule } from "../photo/photo.module";
 import { FilterByDescription } from "./filter-by-description.pipe";
 import { LoadButtonComponent } from "./load-button/load-button.component";
 import { PhotoListComponent } from "./photo-list.component";
 import { PhotosComponent } from "./photos/photos.component";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule {}
